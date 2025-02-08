bd4a406d6f8ae8f4cd5ec5f7acca950e022eccb9
KongTrua <thung260803@gmail.com>
2025-02-08 15:26:18 +0700

    DefaultLayout completed

--- FrontEnd/src/components/NavMenu/index.js
+++ FrontEnd/src/components/NavMenu/index.js
@@ -1,26 +1,40 @@
 import classNames from 'classnames/bind';
 import React from 'react';
 import { Layout, Input, Menu, Button } from 'antd';
-import { SearchOutlined, DownOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+
 import styles from './NavMenu.module.scss';
 
 const cx = classNames.bind(styles);
+const menuItems = [
+    {
+        key: 'home',
+        label: 'Trang chủ',
+        path: '/',
+    },
+    {
+        key: 'search',
+        label: 'Tìm kiếm',
+        path: '/search',
+    },
+    {
+        key: 'profile',
+        label: 'Trang cá nhân',
+        path: '/profile',
+    },
+];
 
 function NavMenu() {
-    return (
-        <Menu className={cx('menu')} mode="horizontal" style={{ borderBottom: 'none', background: 'transparent' }}>
-            <Menu.Item key="university">University</Menu.Item>
-            <Menu.SubMenu
-                key="highschool"
-                title={
-                    <span>
-                        High School <DownOutlined />
-                    </span>
-                }
-            />
-            <Menu.Item key="books">Books</Menu.Item>
-        </Menu>
-    );
+    const navigate = useNavigate();
+
+    const handleOnclick = (e) => {
+        const item = menuItems.find((item) => item.key === e.key);
+        if (item) {
+            navigate(item.path);
+        }
+    };
+
+    return <Menu className={cx('wrapper')} mode="horizontal" items={menuItems} onClick={handleOnclick} />;
 }
-    
+
 export default NavMenu;
