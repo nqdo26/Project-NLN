54ba47e79d4facef694ab22a8b379137bcf10b4d
KongTrua <thung260803@gmail.com>
2025-02-11 18:35:01 +0700

    Admin notification #1

--- FrontEnd/src/pages/AdminNotification/index.js
+++ FrontEnd/src/pages/AdminNotification/index.js
@@ -1,14 +1,82 @@
-import { Divider, Flex, Typography, Input, Button, theme, Steps, message } from 'antd';
+import { Avatar, Button, Flex, List, Skeleton } from 'antd';
 import classNames from 'classnames/bind';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { EditOutlined, ReadOutlined } from '@ant-design/icons';
 
 import styles from './AdminNotification.module.scss';
+import Notification from '~/components/Notification';
 
 function AdminNotification() {
     const cx = classNames.bind(styles);
 
-    return <div className={cx('wrapper')}>admin noti</div>;
+    const count = 3;
+    const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat,picture&noinfo`;
+
+    const [initLoading, setInitLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
+    const [list, setList] = useState([]);
+    useEffect(() => {
+        fetch(fakeDataUrl)
+            .then((res) => res.json())
+            .then((res) => {
+                setInitLoading(false);
+                setData(res.results);
+                setList(res.results);
+            });
+    }, []);
+    const onLoadMore = () => {
+        setLoading(true);
+        setList(
+            data.concat(
+                [...new Array(count)].map(() => ({
+                    loading: true,
+                    name: {},
+                    picture: {},
+                })),
+            ),
+        );
+        fetch(fakeDataUrl)
+            .then((res) => res.json())
+            .then((res) => {
+                const newData = data.concat(res.results);
+                setData(newData);
+                setList(newData);
+                setLoading(false);
+                // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
+                // In real scene, you can using public method of react-virtualized:
+                // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
+                window.dispatchEvent(new Event('resize'));
+            });
+    };
+    const loadMore =
+        !initLoading && !loading ? (
+            <div
+                style={{
+                    textAlign: 'center',
+                    marginTop: 12,
+                    height: 32,
+                    lineHeight: '32px',
+                }}
+            >
+                <Button onClick={onLoadMore}>loading more</Button>
+            </div>
+        ) : null;
+    return (
+        <>
+            <div className={cx('control-bar')}>controls</div>
+            <div className={cx('wrapper')}>
+                <List
+                    className="demo-loadmore-list"
+                    loading={initLoading}
+                    itemLayout="horizontal"
+                    loadMore={loadMore}
+                    dataSource={list}
+                    renderItem={(item) => <Notification item={item} />}
+                />
+            </div>
+        </>
+    );
 }
 
 export default AdminNotification;
