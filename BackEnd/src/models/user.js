a184d61440dde7981eb784c8fbeb32e46e03283d
KongTrua <thung260803@gmail.com>
2025-02-18 17:26:15 +0700

    Models update

--- BackEnd/src/models/user.js
+++ BackEnd/src/models/user.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: String,
     email: String,
     password: String,
-    description: String,
+    fullName: String,
     avatar: String,
-    terms: [{ type: mongoose.Schema.Types.ObjectId, ref: 'term' }],
-    courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'course' }],
-    note: String,
+    isAdmin: Boolean,
+    statistics: {
+        liked: [{ type: mongoose.Schema.Types.ObjectId, ref: 'document' }],
+        disliked: [{ type: mongoose.Schema.Types.ObjectId, ref: 'document' }],
+    },
 });
 
 const User = mongoose.model('user', userSchema);
