383c816d64c59fb57b7732d80f594926b43ff031
Nguyen Quang Do <quangdo0206@gmail.com>
2025-02-18 21:58:52 +0700

    50% Library page

--- FrontEnd/src/pages/Library/index.js
+++ FrontEnd/src/pages/Library/index.js
@@ -1,5 +1,18 @@
+import classNames from 'classnames/bind';
+
+import SearchBar from '~/components/SearchBar';
+import styles from './Library.module.scss';
+import SavedCarousel from '~/components/SavedCarousel';
+const cx = classNames.bind(styles);
+
 function Library() {
-    return <h2>Library page</h2>;
+
+    return (
+        <div className={cx('wrapper')}>
+            <SearchBar />
+            <SavedCarousel title="Your Saved Documents" />
+        </div>
+    );
 }
 
 export default Library;
