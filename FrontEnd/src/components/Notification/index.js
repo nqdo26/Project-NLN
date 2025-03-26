441944390e331c4470d44d3b7304ab05cb7a9dc3
KongTrua <thung260803@gmail.com>
2025-03-26 16:12:23 +0700

    Refactor document deletion logic and update API routes; improve error handling and user feedback

--- FrontEnd/src/components/Notification/index.js
+++ FrontEnd/src/components/Notification/index.js
@@ -1,13 +1,47 @@
-import { List, Skeleton, Avatar, Flex, Button, Badge, Popconfirm } from 'antd';
+import { List, Skeleton, Avatar, Flex, Button, Badge, Popconfirm, message } from 'antd';
 import { useNavigate } from 'react-router-dom';
 
 import CardDocument from '../CardDocument';
+import { deleteDocumentApi, deleteReportApi, updateReportApi } from '~/utils/api';
 
-function Notification({ item }) {
+function Notification({ item, setInitLoading }) {
     const navigate = useNavigate();
 
     const handleOnClick = (path) => {
         navigate('/doc/' + path);
+    };
+
+    const handleUpdateStatus = async (id, status) => {
+        const res = await updateReportApi(id, !status);
+        setInitLoading(true);
+        console.log(res);
+        if (res.EC === 1) {
+            message.success('Cập nhật trạng thái thành công');
+        } else {
+            message.error('Cập nhật trạng thái thất bại');
+        }
+    };
+
+    const handleDeleteReport = async (id) => {
+        const res = await deleteReportApi(id);
+        setInitLoading(true);
+        console.log(res);
+        if (res.EC === 0) {
+            message.success('Xóa báo cáo thành công');
+        } else {
+            message.error('Xóa báo cáo thất bại');
+        }
+    };
+
+    const handleDeleteDocument = async (id) => {
+        const res = await deleteDocumentApi(id);
+        setInitLoading(true);
+        console.log(res);
+        if (res.EC === 0) {
+            message.success('Xóa tài liệu thành công');
+        } else {
+            message.error('Xóa tài liệu thất bại');
+        }
     };
     return (
         <List.Item>
@@ -23,11 +57,11 @@ function Notification({ item }) {
                             {item?.reporterId?.fullName} đã báo cáo về tài liệu
                         </Button>
                     }
-                    description={item.reportAt}
+                    description={item?.reportAt}
                 />
                 <Flex style={{ padding: '10px' }}>{item?.description}</Flex>
                 <Flex gap={10} style={{ padding: '10px' }}>
-                    <Button onClick={() => handleOnClick(item.documentId._id)}>Truy cập tài liệu</Button>
+                    <Button onClick={() => handleOnClick(item?.documentId?._id)}>Truy cập tài liệu</Button>
 
                     <Popconfirm
                         placement="topLeft"
@@ -35,15 +69,27 @@ function Notification({ item }) {
                         description={'Bạn có chắc chắn muốn xóa tài liệu này không?'}
                         okText="Xóa"
                         cancelText="Hủy"
+                        onConfirm={() => handleDeleteDocument(item?.documentId._id)}
                     >
                         <Button danger>Xóa tài liệu</Button>
                     </Popconfirm>
-                    <Button>Đánh dấu đã đọc</Button>
-                    <Button>Xóa thông báo</Button>
+                    <Button onClick={() => handleUpdateStatus(item?._id, item?.status)}>
+                        {item?.status ? 'Đánh dấu chưa đọc' : 'Đánh dấu đã đọc'}
+                    </Button>
+                    <Popconfirm
+                        placement="topLeft"
+                        title={'Xóa tài liệu'}
+                        description={'Bạn có chắc chắn muốn xóa tài liệu này không?'}
+                        okText="Xóa"
+                        cancelText="Hủy"
+                        onConfirm={() => handleDeleteReport(item?._id)}
+                    >
+                        <Button>Xóa thông báo</Button>
+                    </Popconfirm>
                 </Flex>
             </Flex>
-            <Badge.Ribbon color={item.status ? 'blue' : 'red'} text={item.status ? 'Đã đọc' : 'Chưa đọc'}>
-                <CardDocument action="Like" document={item.documentId} />
+            <Badge.Ribbon color={item?.status ? 'blue' : 'red'} text={item?.status ? 'Đã đọc' : 'Chưa đọc'}>
+                <CardDocument action="Like" document={item?.documentId} />
             </Badge.Ribbon>
         </List.Item>
     );
