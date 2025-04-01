34cfae4935e327b16918f2dd9d086221b993de5e
KongTrua <thung260803@gmail.com>
2025-04-01 14:57:49 +0700

    Enhance file upload experience with loading indicator and adjust sidebar actions based on user role

--- FrontEnd/src/components/CustomDragger/index.js
+++ FrontEnd/src/components/CustomDragger/index.js
@@ -1,5 +1,5 @@
 import { InboxOutlined } from '@ant-design/icons';
-import { Button, Flex, message, Upload } from 'antd';
+import { Button, Flex, message, Spin, Upload } from 'antd';
 import { supabase } from '../../utils/supabase'; // Thêm import này
 import { useState } from 'react';
 
@@ -7,9 +7,11 @@ function CustomDragger({ onUploadSuccess, removeLink }) {
     const { Dragger } = Upload;
     const [uploadedName, setUploadedName] = useState('');
     const [fileName, setFileName] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const customRequest = async ({ file, onSuccess, onError, onProgress }) => {
         try {
+            setLoading(true);
             onProgress({ percent: 1 });
             const type = file.name.split('.').pop();
             const uniqueName = `${Date.now()}`;
@@ -26,6 +28,7 @@ function CustomDragger({ onUploadSuccess, removeLink }) {
             setUploadedName(uniqueName);
             setFileName(file.name);
             onUploadSuccess(uniqueName, type);
+            setLoading(false);
         } catch (err) {
             onError(err);
             message.error(`${file.name} upload failed.`);
@@ -44,6 +47,7 @@ function CustomDragger({ onUploadSuccess, removeLink }) {
         multiple: false,
         accept: '.pdf,.doc,.docx,.txt,.ppt,.pptx,.xls,.xlsx',
         customRequest,
+
         onChange(info) {
             const { status } = info.file;
             if (status !== 'uploading') {
@@ -58,18 +62,26 @@ function CustomDragger({ onUploadSuccess, removeLink }) {
 
     return (
         <div>
-            <Dragger showUploadList={false} {...props} disabled={uploadedName !== ''}>
-                <div>
-                    <p className="ant-upload-drag-icon">
-                        <InboxOutlined />
-                    </p>
-                    <p className="ant-upload-text">Nhấp vào hoặc thả file vào đây để tải lên</p>
-                    <p className="ant-upload-hint">
-                        Tải lên một file. Các định dạng tài liệu được hỗ trợ: .pdf, .doc, .docx, .txt, .ppt, .pptx,
-                        .xls, .xlsx
-                    </p>
-                </div>
-            </Dragger>
+            {loading && (
+                <Flex gap={8} style={{ width: '100%' }} justify="center" align="center" vertical>
+                    <Spin></Spin>
+                    <p>Uploading...</p>
+                </Flex>
+            )}
+            {!loading && (
+                <Dragger showUploadList={false} {...props} disabled={uploadedName !== ''}>
+                    <div>
+                        <p className="ant-upload-drag-icon">
+                            <InboxOutlined />
+                        </p>
+                        <p className="ant-upload-text">Nhấp vào hoặc thả file vào đây để tải lên</p>
+                        <p className="ant-upload-hint">
+                            Tải lên một file. Các định dạng tài liệu được hỗ trợ: .pdf, .doc, .docx, .txt, .ppt, .pptx,
+                            .xls, .xlsx
+                        </p>
+                    </div>
+                </Dragger>
+            )}
             <div hidden={uploadedName === ''}>
                 <Flex style={{ width: '100%', padding: '10px' }} justify="space-between" align="center">
                     <p>{fileName}</p>
--- FrontEnd/src/pages/Admin/index.js
+++ FrontEnd/src/pages/Admin/index.js
@@ -1,13 +1,18 @@
-
 import classNames from 'classnames/bind';
 
-
 import styles from './Admin.module.scss';
+import { Flex } from 'antd';
 
 function Admin() {
     const cx = classNames.bind(styles);
 
-    return <div className={cx('wrapper')}>hehe</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <Flex justify="center" align="center" style={{ width: '100%', height: '100%' }}>
+                Truy cập các danh mục để quản lý hệ thống...
+            </Flex>
+        </div>
+    );
 }
 
 export default Admin;
